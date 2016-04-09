43a9f4976899908ef7ff9ac99c2e5467a73a8661
mrtrumpetist <mateus.rocha19@gmail.com>
2016-04-09 07:23:52 -0600

    Created new branch - Mateus.
    Added "add new pet" functionality. Now "animal_listings.php" accesses
    the database searching for the animals, instead of an XML file.

--- animal_listings.php
+++ animal_listings.php
@@ -7,6 +7,7 @@
       $title = "Adopt";
       $session_name = "PetRescue_Malmstrom_Bertolacci";
       include 'header.php';
+	  include 'lib/database.php';
       include "animal_tools.php";
     ?>
   </head>
@@ -19,6 +20,25 @@
       ?>
       <div class="about_us_header"><h2 class="abtUsText">Our Animals:</h2></div>
       <?php
+		$db = new database();
+	  $result = $db->query('SELECT * FROM animals');
+
+		foreach($result as $row)
+		{?>
+			<p>
+			<img class="animalPhoto" src="<?php echo $row['pet_image_path'];?>" width="300px" height="300px" /><br>
+			Name: <?php echo $row['pet_name'];?><br>
+			Species: <?php echo $row['pet_species'];?><br>
+			Breed: <?php echo $row['pet_breed'];?><br>
+			Age: <?php echo $row['pet_age'];?> yrs<br>
+			Weight: <?php echo $row['pet_weight'];?> lbs<br>
+			Description: <?php echo $row['pet_description'];?><br>
+			<a href=<?php echo 'animal_view.php?id='. $row['id']; ?> >Click here to view</a>
+			</p>
+			<hr>
+		<?php
+		}
+	  /*
         $animal_tree = new AnimalTree( $ANIMALS_FILE );
 
         echo array_reduce( $animal_tree->getIterable(),
@@ -27,10 +47,14 @@
                               "<a class=\"animalView\" href=\"animal_view.php?id=".$item->getID()."\">Click here to view " . $item->getName() . "!</a> \n" ;
                             }
                           );
+		*/
+		
+		
 
         /*foreach( $animal_tree->getIterable() as $pet ){
           echo $pet;
         }*/
+		
       ?>
 
     </div>
--- navbar.php
+++ navbar.php
@@ -4,7 +4,8 @@
     <nav>
       <a href="index.php">Home</a>  |
       <a href="animal_listings.php">Animals</a>  |
+	  <a href="adoption.php">Add pet for adoption</a> |
       <a href="about.php">About Us</a>  |
       <a href="login.php">Login</a>  |
-    </nav>
+  </nav>
   </header>
