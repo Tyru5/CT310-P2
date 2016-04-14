712eb349b1f3a35978d2d857288a5f09bc809b4a
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-04-14 10:42:39 -0600

    made the search form not autocomplete searchings

--- animal_listings.php
+++ animal_listings.php
@@ -49,7 +49,7 @@
       <div class="about_us_header"><h2 class="abtUsText">Our Animals:</h2></div>
       <!--Assignment #2 AJAX live search for any animal in our database::tyru5 -->
       <form>
-          <input id="search_animal" type="text" size="30" placeholder="Search Animal" onkeyup="showAnimal(this.value)">
+          <input id="search_animal" type="text" size="30" placeholder="Search Animal" onkeyup="showAnimal(this.value)" autocomplete="off">
           <div id="livesearch"></div>
       </form>
       <?php
